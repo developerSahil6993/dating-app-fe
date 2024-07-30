1e0718dcd203dcb569874065e939cb861c18e7de
Sahil Shrivastava <skshrivastava1234@gmail.com>
2024-07-30 22:59:36 +0530

    added profile selector module

--- dating-ai-fe/src/App.jsx
+++ dating-ai-fe/src/App.jsx
@@ -1,9 +1,48 @@
 import "./App.css";
+import { User, MessageCircle, X, Heart } from "lucide-react";
+
+const ProfileSelector = () => {
+  return (
+    <>
+      <div className="rounded-lg overflow-hidden bg-white shadow-lg">
+        <div className="relative">
+          <img src="http://localhost:8080/images/02e49b90-499c-4d3f-97f2-42c63b81ba79.jpg" />
+          <div className="absolute bottom-0 left-0 right-0 text-white p-4 bg-gradient-to-r from-green-500">
+            <h2 className="text-3xl font-bold">Foo bar, 30</h2>
+          </div>
+        </div>
+        <div className="p-4">
+          <p className="text-gray-600">Some Bio of User!</p>
+        </div>
+        <div className="flex p-4 justify-center space-x-4">
+          <button
+            className="bg-red-500 rounded-full p-4 hover:bg-red-700"
+            onClick={() => console.log("Reject!")}
+          >
+            <X size={24} />
+          </button>
+          <button
+            className="bg-green-500 rounded-full p-4 hover:bg-green-700"
+            onClick={() => console.log("%c sucess", "color:green")}
+          >
+            <Heart size={24} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <div className="max-w-md mx-auto">
+        <nav className="flex justify-between">
+          <User />
+          <MessageCircle />
+        </nav>
+        <ProfileSelector />
+      </div>
     </>
   );
 }
