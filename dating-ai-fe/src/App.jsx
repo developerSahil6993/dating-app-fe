f3e11d8578f3500a94c5d1c57b36f476749fe469
Sahil Shrivastava <skshrivastava1234@gmail.com>
2024-08-02 22:41:25 +0530

    Added matches list component

--- dating-ai-fe/src/App.jsx
+++ dating-ai-fe/src/App.jsx
@@ -33,6 +33,44 @@ const ProfileSelector = () => {
   );
 };
 
+const MatchesList = () => (
+  <div className="rounded-lg shadow-lg p-4">
+    <h2 className="text-2xl font-bold mb-4">Matches</h2>
+    <ul>
+      {[
+        {
+          id: 1,
+          firstName: "Foo",
+          lastName: "Bar",
+          imageUrl:
+            "http://localhost:8080/images/02e49b90-499c-4d3f-97f2-42c63b81ba79.jpg",
+        },
+        {
+          id: 2,
+          firstName: "Foo2",
+          lastName: "Bar2",
+          imageUrl:
+            "http://localhost:8080/images/02e49b90-499c-4d3f-97f2-42c63b81ba79.jpg",
+        },
+      ].map((match) => (
+        <li key={match.id} className="mb-2">
+          <button className="w-full hover:bg-gray-100 rounded flex item-center">
+            <img
+              src={match.imageUrl}
+              className="w-16 h-16 rounded-full mr-3 object-cover"
+            />
+            <span>
+              <h3 className="text-xl font-bold">
+                {match.firstName} {match.lastName}
+              </h3>
+            </span>
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
 function App() {
   return (
     <>
@@ -41,7 +79,8 @@ function App() {
           <User />
           <MessageCircle />
         </nav>
-        <ProfileSelector />
+        {/* <ProfileSelector /> */}
+        <MatchesList />
       </div>
     </>
   );
